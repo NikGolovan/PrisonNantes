c7ac72819438debc00eeef0e9fd4bf18fdf5d783
golovan <nicolas.golovan@datavirtuality.de>
2020-11-15 13:30:56 +0100

    - changes

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        var sideBarClosed = $('#sidebar').hasClass("active");
+        if (!sideBarClosed) {
+            $('#sidebarCollapse span').text('Fermer la barre latérale');
+        } else {
+            $('#sidebarCollapse span').text('Ouvrir la barre latérale');
+        }
+    });
+});
