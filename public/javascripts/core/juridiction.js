76db8ce24d3dbcce94eb3d6eece9f973d61d7ab7
golovan <nicolas.golovan@datavirtuality.de>
2020-11-15 16:50:19 +0100

    - added some core files

--- public/javascripts/core/juridiction.js
+++ public/javascripts/core/juridiction.js
@@ -3,8 +3,9 @@
  */
 const Juridiction = function (options) {
     this.nom_juridiction = options.nom_juridiction;
+    this.n_affaire = options.n_affaire;
 }
 
-const juridiction = new Juridiction({nom_juridiction: 'nom juridiction'});
+const juridiction = new Juridiction({n_affaire: 1, nom_juridiction: 'nom juridiction'});
 console.log(juridiction);
 
