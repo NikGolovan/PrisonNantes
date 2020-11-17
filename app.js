64168f89299ee619e6c731330b0f3b8384bb11ac
Jorge Ara <jorgito98ara@gmail.com>
2020-11-17 20:35:37 +0100

    ajout de decision et preventive

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var incarcerateRouter = require('./routes/incarcerate');
 var decisionRouter = require('./routes/decision');
+var preventiveRouter = require('./routes/preventive');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/incarcerate', incarcerateRouter);
 app.use('/decision', decisionRouter);
+app.use('/preventive', preventiveRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/preventive.js
+++ routes/preventive.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var dbConn  = require('../lib/db');
 
 router.get('/', function(req, res, next) {
-    dbConn.all('SELECT * FROM Decision ORDER BY n_ecrou desc', function(err,rows) {
+    dbConn.all('SELECT * FROM Detenu ORDER BY n_ecrou desc', function(err,rows) {
         if(err) {
             req.flash('error', err);
-            res.render('pages/decision',{data:''});
+            res.render('pages/preventive',{data:''});
         } else {
-            res.render('pages/decision',{
+            res.render('pages/preventive',{
                 data: rows,
-                n_type_decision: '',
                 n_ecrou: '',
-                date_decision: ''
+                prenom: '',
+                nom: ''
             });
         }
     });
@@ -20,28 +20,28 @@ router.get('/', function(req, res, next) {
 
 // prendre decision
 router.post('/', function(req, res, next) {
-    let n_type_decision = req.body.n_type_decision;
     let n_ecrou = req.body.n_ecrou;
-    let date_decision = req.body.date_decision;
+    let prenom = req.body.prenom;
+    let nom = req.body.nom;
     let errors = false;
 
-    if(n_type_decision.length === 0 || n_ecrou.length === 0 || date_decision.length === 0 ) {
+    if(n_ecrou.length === 0 || prenom.length === 0 || nom.length === 0 ) {
 
         errors = true;
 
         // set flash message
         req.flash('error', "Veuillez saisir tous les champs.");
         // render to add.ejs with flash message
-        dbConn.all('SELECT * FROM Decision ORDER BY n_ecrou desc', function(err,rows) {
+        dbConn.all('SELECT * FROM Detenu ORDER BY n_ecrou desc', function(err,rows) {
             if(err) {
                 req.flash('error', err);
-                res.render('pages/incarcerate',{data:''});
+                res.render('pages/preventive',{data:''});
             } else {
-                res.render('pages/incarcerate',{
+                res.render('pages/preventive',{
                     data: rows,
-                    n_type_decision: n_type_decision,
                     n_ecrou: n_ecrou,
-                    date_decision: date_decision
+                    prenom: prenom,
+                    nom: nom
                 });
             }
         });
@@ -51,9 +51,9 @@ router.post('/', function(req, res, next) {
     if(!errors) {
 
         var form_data = {
-            $n_type_decision: n_type_decision,
             $n_ecrou: n_ecrou,
-            $date_decision: date_decision
+            $prenom: prenom,
+            $nom: nom
         }
 
         // insert query
@@ -63,16 +63,16 @@ router.post('/', function(req, res, next) {
                 let erreurMsg = err.toString().indexOf('UNIQUE CONSTRAINT FAILED') ? "La decision avec le numéro " + n_ecrou + " déjà existe." : err;
                 req.flash('error', erreurMsg)
                 // render to add.ejs
-                dbConn.all('SELECT * FROM Decision ORDER BY n_ecrou desc', function(err,rows) {
+                dbConn.all('SELECT * FROM Detenu ORDER BY n_ecrou desc', function(err,rows) {
                     if(err) {
                         req.flash('error', err);
-                        res.render('pages/decision',{data:''});
+                        res.render('pages/preventive',{data:''});
                     } else {
-                        res.render('pages/decision',{
+                        res.render('pages/preventive',{
                             data: rows,
-                            n_type_decision: n_type_decision,
                             n_ecrou: n_ecrou,
-                            date_decision: date_decision
+                            prenom: date_decision,
+                            nom: n_type_decision
                         });
                     }
                 });
