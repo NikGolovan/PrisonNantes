4429bb80df27c184fc81b2199271ddcc67d1cf39
golovan <nicolas.golovan@datavirtuality.de>
2020-11-22 21:26:02 +0100

    - doutable fix

--- public/javascripts/core/detenu.js
+++ public/javascripts/core/detenu.js
@@ -1,14 +1,12 @@
 /*
     La classe Detenu
  */
-const Detenu = function (options) {
-    this.n_ecrou = options.n_ecrou;
-    this.prenom = options.prenom;
-    this.nom = options.nom;
-    this.date_naissance = options.date_naissance;
-    this.lieu_naissance = options.lieu_naissance;
+const Detenu = function (n_ecrou, prenom, nom, date_naissance, lieu_naissance) {
+    this.n_ecrou = n_ecrou;
+    this.prenom = prenom;
+    this.nom = nom;
+    this.date_naissance = date_naissance;
+    this.lieu_naissance = lieu_naissance;
 }
 
-const detenu = new Detenu({n_ecrou: 1, prenom: 'Prenom', nom: 'Nom', date_naissance: '3/3/3', lieu_naissance: 'France'});
-console.log(detenu);
-
+module.exports = Detenu;
