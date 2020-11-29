6c3a79d57f79474028185c4188950e69d54e2b1a
golovan <nicolas.golovan@datavirtuality.de>
2020-11-29 22:04:27 +0100

    - fixes

--- routes/liberer_definitivement.js
+++ routes/liberer_definitivement.js
@@ -109,4 +109,82 @@ router.post('/', function(req, res, next) {
         })
     }
 })
+
+// display edit book page
+router.get('/edit/(:n_ecrou)', function (req, res, next) {
+    let n_ecrou = req.params.n_ecrou;
+    let query = "SELECT * FROM Liberation_definitive WHERE n_ecrou = '" + n_ecrou + "'";
+
+    dbConn.all(query, function (err, rows, fields) {
+        if (err) throw err
+        if (rows.length <= 0) {
+            req.flash('error', 'Pas de condamné avec n_ecrou = ' + n_ecrou)
+            res.redirect('pages/liberer')
+        } else {
+            res.render('pages/edit_liberation', {
+                title: 'Modifier Information',
+                n_type_decision: rows[0].n_type_decision,
+                n_ecrou: rows[0].n_ecrou,
+                date_decision: rows[0].date_decision,
+                date_liberation: rows[0].date_liberation,
+                canceled: ''
+            })
+        }
+    })
+})
+
+router.post('/update/:n_ecrou', function (req, res, next) {
+    let fields = {
+        n_ecrou: req.params.n_ecrou,
+        date_decision: req.body.date_decision,
+        date_liberation: req.body.date_liberation
+    }
+    let canceled = req.body.canceled;
+    let errors = false;
+
+    if (canceled) {
+        res.redirect('/liberer');
+        return;
+    }
+
+    if (!allFieldsAreSet(fields)) {
+        errors = true;
+        req.flash('error', "Veuillez saisir les modifications.");
+        res.render('pages/edit_liberation', {
+            n_type_decision: req.params.n_type_decision,
+            n_ecrou: req.params.n_ecrou,
+            date_decision: req.body.date_decision,
+            date_liberation: req.body.date_liberation
+        })
+    }
+
+    if (!errors) {
+        var form_data = {
+            $date_decision: req.body.date_decision,
+            $date_liberation: req.body.date_liberation
+        }
+
+        dbConn.run("UPDATE Liberation_definitive SET date_decision = $date_decision, date_liberation = $date_liberation WHERE n_ecrou = '" + fields["n_ecrou"] + "'", form_data, function (err, result) {
+            if (err) {
+                req.flash('error', err)
+                res.render('pages/edit_liberation', {
+                    date_decision: req.body.date_decision,
+                    duree: req.body.duree
+                })
+            } else {
+                req.flash('success', 'Les informtions ont été bien mises à jour.');
+                res.redirect('/liberer');
+            }
+        })
+    }
+})
+
+function allFieldsAreSet(fields) {
+    for (let key in fields) {
+        if (fields[key].length === 0)
+            return false;
+    }
+    return true;
+}
+
 module.exports = router;
