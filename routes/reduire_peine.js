c1e8517df347478e8099a15daa35c5a8fa9c8885
golovan <nicolas.golovan@datavirtuality.de>
2020-12-15 19:48:42 +0100

    - fixes

--- routes/reduire_peine.js
+++ routes/reduire_peine.js
@@ -29,12 +29,10 @@ router.post('/', function (req, res, next) {
     let n_ecrou = req.body.n_ecrou;
     let date_decision = req.body.date_decision;
     let duree = req.body.duree;
-    let errors = false;
 
     if (n_ecrou.length === 0 || n_type_decision.length === 0 ||
         date_decision.length === 0 || duree.length === 0) {
 
-        errors = true;
         req.flash('error', "Veuillez saisir tous les champs.");
 
         dbConn.all('SELECT * FROM Reduction_peine ORDER BY n_ecrou desc', function (err, rows) {
@@ -53,68 +51,65 @@ router.post('/', function (req, res, next) {
         });
     }
 
-    if (!errors) {
-
-        var form_data = {
-            $n_type_decision: n_type_decision,
-            $n_ecrou: n_ecrou,
-            $date_decision: date_decision,
-            $duree: duree
-        }
-
-        var form_data_decision = {
-            $n_type_decision: n_type_decision,
-            $n_ecrou: n_ecrou,
-            $date_decision: date_decision,
-        }
-
-        let queryInsert = "INSERT INTO Reduction_peine values ($n_type_decision, $n_ecrou, $date_decision, $duree)";
-        let queryCheckId = "SELECT \"n_ecrou\" FROM Detenu WHERE n_ecrou = '" + n_ecrou + "'";
-        let queryInsertDecision = "INSERT OR IGNORE INTO Decision values ($n_type_decision, $n_ecrou, $date_decision)";
-
-        logger.infoCreateReductionPeine(req.body.n_ecrou);
-        logger.infoExecQuery();
-        dbConn.all(queryCheckId, function (err, result) {
-            if (err) throw err;
-            dbConn.all(queryInsertDecision, form_data_decision, function (err, result) {
-            })
-            if (result.length > 0) {
-                // insert query
-                dbConn.all(queryInsert, form_data, function (err, result) {
-                    if (err) {
-                        let erreurMsg = err.toString().indexOf('UNIQUE CONSTRAINT FAILED') ? "L'incarcéré avec le numéro " + n_ecrou + " déjà existe." : err;
-                        req.flash('error', erreurMsg)
-                        dbConn.all('SELECT * FROM Reduction_peine ORDER BY n_ecrou desc', function (err, rows) {
-                            if (err) {
-                                req.flash('error', err);
-                                res.render('pages/reduire_peine', {data: ''});
-                            } else {
-                                res.render('pages/reduire_peine', {
-                                    data: rows,
-                                    n_type_decision: '',
-                                    n_ecrou: '',
-                                    date_decision: '',
-                                    duree: ''
-                                });
-                            }
-                        });
-                    } else {
-                        req.flash('success', 'Une nouvelle reduction de peine a été bien enregistré.');
-                        res.redirect(req.get('referer'));
-                    }
-                })
-            } else {
-                req.flash('error', "Detenu avec le numero " + n_ecrou + " n'existe pas.");
-                res.redirect('/reduire');
-                return;
-            }
-        })
-        let queryUpdateDuree = "UPDATE Condamnation SET duree = duree - " + duree + " WHERE n_ecrou = '" + n_ecrou + "' AND duree > " + duree;
-        dbConn.all(queryUpdateDuree, function (err) {
-            if (err) throw err;
-        })
-        logger.infoReductionPeineSuccess(req.body.n_ecrou);
+    var form_data = {
+        $n_type_decision: n_type_decision,
+        $n_ecrou: n_ecrou,
+        $date_decision: date_decision,
+        $duree: duree
     }
+
+    var form_data_decision = {
+        $n_type_decision: n_type_decision,
+        $n_ecrou: n_ecrou,
+        $date_decision: date_decision,
+    }
+
+    let queryInsert = "INSERT INTO Reduction_peine values ($n_type_decision, $n_ecrou, $date_decision, $duree)";
+    let queryCheckId = "SELECT \"n_ecrou\" FROM Detenu WHERE n_ecrou = '" + n_ecrou + "'";
+    let queryInsertDecision = "INSERT OR IGNORE INTO Decision values ($n_type_decision, $n_ecrou, $date_decision)";
+
+    logger.infoCreateReductionPeine(req.body.n_ecrou);
+    logger.infoExecQuery();
+    dbConn.all(queryCheckId, function (err, result) {
+        if (err) throw err;
+        dbConn.all(queryInsertDecision, form_data_decision, function (err, result) {
+        })
+        if (result.length > 0) {
+            // insert query
+            dbConn.all(queryInsert, form_data, function (err, result) {
+                if (err) {
+                    let erreurMsg = err.toString().indexOf('UNIQUE CONSTRAINT FAILED') ? "L'incarcéré avec le numéro " + n_ecrou + " déjà existe." : err;
+                    req.flash('error', erreurMsg)
+                    dbConn.all('SELECT * FROM Reduction_peine ORDER BY n_ecrou desc', function (err, rows) {
+                        if (err) {
+                            req.flash('error', err);
+                            res.render('pages/reduire_peine', {data: ''});
+                        } else {
+                            res.render('pages/reduire_peine', {
+                                data: rows,
+                                n_type_decision: '',
+                                n_ecrou: '',
+                                date_decision: '',
+                                duree: ''
+                            });
+                        }
+                    });
+                } else {
+                    req.flash('success', 'Une nouvelle reduction de peine a été bien enregistré.');
+                    res.redirect(req.get('referer'));
+                }
+            })
+        } else {
+            req.flash('error', "Detenu avec le numero " + n_ecrou + " n'existe pas.");
+            res.redirect('/reduire');
+            return;
+        }
+    })
+    let queryUpdateDuree = "UPDATE Condamnation SET duree = duree - " + duree + " WHERE n_ecrou = '" + n_ecrou + "' AND duree > " + duree;
+    dbConn.all(queryUpdateDuree, function (err) {
+        if (err) throw err;
+    })
+    logger.infoReductionPeineSuccess(req.body.n_ecrou);
 })
 
 /* Afficher la page de modification des informations */
